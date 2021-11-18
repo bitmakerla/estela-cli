1dfe6cbe329441631e73961725a490be2f6f840a
Alonso-Valdivia <34718875+Adriana618@users.noreply.github.com>
2021-11-18 08:28:54 -0500

    BITMAKER-1261: Improve Readme (#6)
    * BITMAKER-1261: Improve Readme, also modified commands to get data all from job and filter jobs for tag
    
    * BITMAKER-1261: Removed cronjobs from actions that we can do with bitmaker client

--- bm_cli/list/job.py
+++ bm_cli/list/job.py
@@ -2,7 +2,7 @@ import click
 
 from tabulate import tabulate
 from bm_cli.login import login
-from bm_cli.utils import get_bm_settings, format_time, format_key_value_pairs
+from bm_cli.utils import get_bm_settings, format_time, format_key_value_pairs, format_tags
 
 SHORT_HELP = "List the spider's jobs"
 
@@ -10,12 +10,19 @@ SHORT_HELP = "List the spider's jobs"
 @click.command(short_help=SHORT_HELP)
 @click.argument("sid", required=True)
 @click.argument("pid", required=False)
-def bm_command(sid, pid):
+@click.option(
+    "-t",
+    "--tag",
+    type=click.UNPROCESSED,
+    help="Filter jobs by tag",
+)
+def bm_command(sid, pid, tag):
     """List jobs of a given spider
 
     \b
     SID is the spider's sid
     PID is the project's pid (active project by default)
+    TAG is tags to filter jobs
     """
 
     bm_client = login()
@@ -36,18 +43,23 @@ def bm_command(sid, pid):
             "The spider does not exist, or you do not have permission to perform this action."
         )
 
-    jobs = bm_client.get_spider_jobs(pid, sid)
+    jobs = []
+    if tag:
+        jobs = bm_client.get_spider_jobs_with_tag(pid, sid, tag)
+    else:
+        jobs = bm_client.get_spider_jobs(pid, sid)
+
     jobs = [
         [
             job["jid"],
             job["job_status"].capitalize(),
+            format_tags(job["tags"]),
             format_key_value_pairs(job["args"]),
             format_key_value_pairs(job["env_vars"]),
             format_time(job["created"]),
         ]
         for job in jobs
-        if job["job_type"] == "SINGLE_JOB"
     ]
 
-    headers = ["JID", "STATUS", "ARGS", "ENV VARS", "CREATED"]
+    headers = ["JID", "STATUS", "TAGS", "ARGS", "ENV VARS", "CREATED"]
     click.echo(tabulate(jobs, headers, numalign="left", tablefmt="plain"))
